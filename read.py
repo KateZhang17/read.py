60741f926343ea27830dde4c7874e7d6eaca9736
Jiachen ZHANG <jzhangea@connect.ust.hk>
2025-02-27 01:22:32 +0800

    calculate average length of reviews

---
+++ read.py
@@ -0,0 +1,17 @@
+data = []
+count = 0
+with open('reviews.txt', 'r') as f:
+	for line in f:
+		data.append(line)
+		count += 1
+		if count % 1000 == 0:
+			print(len(data))
+
+print('Finished reading the reviews, there are', len(data), 'reviews in total.')
+
+
+sum_len = 0
+for d in data:
+	sum_len += len(d)
+
+print('The average length of reviews is', sum_len/len(data))
